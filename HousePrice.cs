81882a7716a5ad964c229d679bad9fe70e0e5434
Andy Garner <andy.garner@elasticmint.com>
2021-02-20 22:05:49 +0000

    Add project files.

---
+++ HousePrice.cs
@@ -0,0 +1,184 @@
+﻿using Microsoft.ML.Data;
+
+namespace HousePricePrediction
+{
+    public class ToAreaInThousands
+    {
+        public float LotAreaThousands { get; set; }
+        public float TotalBasementSquareFeetThousands { get; set; }
+        public float GroundLivingAreaThousands { get; set; }
+        public float GarageAreaThousands { get; set; }
+    }
+
+    public class HousePricePrediction
+    {
+        [ColumnName("Score")]
+        public float HousePrice { get; set; }
+    }
+
+    public class HousePrice
+    {
+        //[LoadColumn(0)]
+        //public string Id { get; set; }
+        [LoadColumn(1)]
+        public string MSSubClass { get; set; }
+        //[LoadColumn(2)]
+        //public string MSZoning { get; set; }
+        [LoadColumn(3)] // Can be NA but usually a number
+        public float LotFrontage { get; set; }
+        [LoadColumn(4)]
+        public float LotArea { get; set; }
+        //[LoadColumn(5)]
+        //public string Street { get; set; }
+        //[LoadColumn(6)]
+        //public string Alley { get; set;}
+        //[LoadColumn(7)]
+        //public string LotShape { get; set; }
+        //[LoadColumn(8)]
+        //public string LandContour { get; set; }
+        //[LoadColumn(9)]
+        //public string Utilities { get; set;}
+        //[LoadColumn(10)]
+        //public string LotConfig { get; set;}
+        //[LoadColumn(11)]
+        //public string LandSlope { get; set;}
+        [LoadColumn(12)]
+        public string Neighbourhood { get; set; }
+        //[LoadColumn(13)]
+        //public string Condition1 { get; set; }
+        //[LoadColumn(14)]
+        //public string Condition2 { get; set; }
+        [LoadColumn(15)]
+        public string BuildingType { get; set; }
+        [LoadColumn(16)]
+        public string HouseStyle { get; set; }
+        [LoadColumn(17)]
+        public float OverallQuality { get; set; }
+        [LoadColumn(18)]
+        public float OverallCondition { get; set; }
+        [LoadColumn(19)]
+        public float YearBuilt { get; set; }
+        [LoadColumn(20)]
+        public float YearRemodAdd { get; set; }
+        //[LoadColumn(21)]
+        //public string RoofStyle { get; set; }
+        //[LoadColumn(22)]
+        //public string RoofMatl { get; set; }
+        [LoadColumn(23)]
+        public string Exterior1st { get; set; }
+        [LoadColumn(24)]
+        public string Exterior2nd { get; set; }
+        //[LoadColumn(25)]
+        //public string MasVnrType { get; set; }
+        //[LoadColumn(26)]
+        //public int MasVnrArea { get; set; }
+        //[LoadColumn(27)]
+        //public string ExterQual { get; set; }
+        //[LoadColumn(28)]
+        //public string ExterCond { get; set; }
+        [LoadColumn(29)]
+        public string Foundation { get; set; }
+        //[LoadColumn(30)]
+        //public string BsmtQual { get; set; }
+        //[LoadColumn(31)]
+        //public string BsmtCond { get; set; }
+        //[LoadColumn(32)]
+        //public string BsmtExposure { get; set; }
+        //[LoadColumn(33)]
+        //public string BsmtFinType1 { get; set; }
+        //[LoadColumn(34)]
+        //public int BsmtFinSF1 { get; set; }
+        //[LoadColumn(35)]
+        //public string BsmtFinType2 { get; set; }
+        //[LoadColumn(36)]
+        //public int BsmtFinSF2 { get; set; }
+        //[LoadColumn(37)]
+        //public int BsmtUnfSF { get; set; }
+        [LoadColumn(38)]
+        public float TotalBasementSquareFeet { get; set; }
+        //[LoadColumn(39)]
+        //public string Heating { get; set; }
+        //[LoadColumn(40)]
+        //public string HeatingQC { get; set; }
+        //[LoadColumn(41)]
+        //public string CentralAir { get; set; }
+        //[LoadColumn(42)]
+        //public string Electrical { get; set; }
+        //[LoadColumn(43)]
+        //public int FirstFlrSF { get; set; }
+        //[LoadColumn(44)]
+        //public int SecondFlrSF { get; set; }
+        //[LoadColumn(45)]
+        //public int LowQualFinSF { get; set; }
+        [LoadColumn(46)]
+        public float GroundLivingArea { get; set; }
+        //[LoadColumn(47)]
+        //public int BsmtFullBath { get; set; }
+        //[LoadColumn(48)]
+        //public int BsmtHalfBath { get; set; }
+        [LoadColumn(49)]
+        public float FullBath { get; set; }
+        //[LoadColumn(50)]
+        //public int HalfBath { get; set; }
+        [LoadColumn(51)]
+        public float BedroomsAboveGround { get; set; }
+        //[LoadColumn(52)]
+        //public int KitchenAbvGr { get; set; }
+        [LoadColumn(53)]
+        public string KitchenQuality { get; set; }
+        [LoadColumn(54)]
+        public float TotalRoomsAboveGround { get; set; }
+        //[LoadColumn(55)]
+        //public string Functional { get; set; }
+        //[LoadColumn(56)]
+        //public int Fireplaces { get; set; }
+        //[LoadColumn(57)]
+        //public string FireplaceQu { get; set; }
+        [LoadColumn(58)]
+        public string GarageType { get; set; }
+        //[LoadColumn(59)]
+        //public string GarageYrBlt { get; set; }
+        [LoadColumn(60)]
+        public string GarageFinish { get; set; }
+        [LoadColumn(61)]
+        public float GarageCars { get; set; }
+        [LoadColumn(62)]
+        public float GarageArea { get; set; }
+        //[LoadColumn(63)]
+        //public string GarageQual { get; set; }
+        //[LoadColumn(64)]
+        //public string GarageCond { get; set; }
+        //[LoadColumn(65)]
+        //public string PavedDrive { get; set; }
+        //[LoadColumn(66)]
+        //public int WoodDeckSF { get; set; }
+        //[LoadColumn(67)]
+        //public int OpenPorchSF { get; set; }
+        //[LoadColumn(68)]
+        //public int EnclosedPorch { get; set; }
+        //[LoadColumn(69)]
+        //public int ThreeSsnPorch { get; set; }
+        //[LoadColumn(70)]
+        //public int ScreenPorch { get; set; }
+        //[LoadColumn(71)]
+        //public int PoolArea { get; set; }
+        //[LoadColumn(72)]
+        //public string PoolQC { get; set; }
+        //[LoadColumn(73)]
+        //public string Fence { get; set; }
+        //[LoadColumn(74)]
+        //public string MiscFeature { get; set; }
+        //[LoadColumn(75)]
+        //public int MiscVal { get; set; }
+        //[LoadColumn(76)]
+        //public int MoSold { get; set; }
+        //[LoadColumn(77)]
+        //public string YrSold { get; set; }
+        //[LoadColumn(78)]
+        //public string SaleType { get; set; }
+        //[LoadColumn(79)]
+        //public string SaleCondition { get; set; }
+        [LoadColumn(80)]
+        public float SalePrice { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using BetterConsoleTables;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using Microsoft.ML.Trainers.FastTree;
+
+namespace HousePricePrediction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new MLContext();
+
+            var data = LoadData(context);
+
+            var pipeline = BuildPipeline(context);
+
+            //Preview(pipeline, data);
+
+            var model = TrainModel(context, data, pipeline);
+
+            PredictPrices(context, model);
+
+            Console.WriteLine("Press enter to finish");
+            Console.Read();
+        }
+
+        private static void Preview(
+            IEstimator<ITransformer> pipeline,
+            IDataView data)
+        {
+            var model = pipeline.Fit(data);
+            var transformedData = model.Transform(data);
+            var preview = transformedData.Preview(maxRows: 10);
+            WritePreview(preview);
+        }
+
+        public static void WritePreview(DataDebuggerPreview preview)
+        {
+            // set up a console table
+            var table = new Table(
+                TableConfiguration.Unicode(),
+                (from c in preview.ColumnView 
+                    select new ColumnHeader(c.Column.Name)).ToArray());
+
+            // fill the table with results
+            foreach (var row in preview.RowView)
+            {
+                table.AddRow((from c in row.Values 
+                        select c.Value is VBuffer<float> ? "<vector>" : c.Value
+                    ).ToArray());
+            }
+
+            // write the table
+            Console.WriteLine(table.ToString());
+        }
+
+        private static IDataView LoadData(
+            MLContext context)
+        {
+            Console.Write("Loading data...");
+
+            var dataPath = Path.Combine(Environment.CurrentDirectory, "data.csv");
+            var textLoader = context.Data.CreateTextLoader(
+                new TextLoader.Options
+                {
+                    Separators = new[] {','},
+                    HasHeader = true,
+                    Columns = new[]
+                    {
+                        new TextLoader.Column("SalePrice", DataKind.Single, 80),
+                        new TextLoader.Column("LotArea", DataKind.Single, 4),
+                        new TextLoader.Column("OverallQuality", DataKind.Single, 17),
+                        new TextLoader.Column("OverallCondition", DataKind.Single, 18),
+                        new TextLoader.Column("YearBuilt", DataKind.Single, 19),
+                        new TextLoader.Column("YearRemodAdd", DataKind.Single, 20),
+                        new TextLoader.Column("YearSold", DataKind.Single, 77),
+                        new TextLoader.Column("MSSubClass", DataKind.String, 1),
+                        new TextLoader.Column("Neighbourhood", DataKind.String, 12),
+                        new TextLoader.Column("TotalBasementSquareFeet", DataKind.Single, 38),
+                        new TextLoader.Column("GroundLivingArea", DataKind.Single, 46),
+                        new TextLoader.Column("FullBath", DataKind.Single, 49),
+                        new TextLoader.Column("TotalRoomsAboveGround", DataKind.Single, 54),
+                        new TextLoader.Column("BedroomsAboveGround", DataKind.Single, 51),
+                        new TextLoader.Column("GarageCars", DataKind.Single, 61),
+                        new TextLoader.Column("BuildingType", DataKind.String, 15),
+                        new TextLoader.Column("HouseStyle", DataKind.String, 16),
+                        new TextLoader.Column("KitchenQuality", DataKind.String, 53),
+                        new TextLoader.Column("GarageArea", DataKind.Single,62),
+                        new TextLoader.Column("Foundation", DataKind.String, 29),
+                        new TextLoader.Column("Exterior1st", DataKind.String, 23),
+                        new TextLoader.Column("Exterior2nd", DataKind.String, 24),
+                        new TextLoader.Column("LotFrontage", DataKind.Single, 3),
+                        new TextLoader.Column("GarageFinish", DataKind.String, 60),
+                        new TextLoader.Column("GarageType", DataKind.String, 58)
+                    }
+                });
+
+            var data = textLoader.Load(dataPath);
+
+            Console.WriteLine("done!");
+
+            return data;
+        }
+
+        private static IEstimator<ITransformer> BuildPipeline(
+            MLContext context)
+        {
+            Console.Write("Building pipeline...");
+
+            var pipeline =
+                context.Transforms
+                    .CopyColumns(inputColumnName: "SalePrice", outputColumnName: "Label")
+                    .Append(context.Transforms.CustomMapping<HousePrice, ToAreaInThousands>((
+                        input,
+                        output) =>
+                    {
+                        output.LotAreaThousands = input.LotArea / 1000;
+                        output.GroundLivingAreaThousands = input.GroundLivingArea / 1000;
+                        output.TotalBasementSquareFeetThousands = input.TotalBasementSquareFeet / 1000;
+                        output.GarageAreaThousands = input.GarageArea / 1000;
+                    }, contractName: "AreaInThousands"))
+                    //.Append(context.Transforms.Categorical.OneHotEncoding("MSSubClass"))
+                    .Append(context.Transforms.Categorical.OneHotEncoding("Neighbourhood"))
+                    .Append(context.Transforms.Categorical.OneHotEncoding("BuildingType"))
+                    .Append(context.Transforms.Categorical.OneHotEncoding("HouseStyle"))
+                    .Append(context.Transforms.Categorical.OneHotEncoding("KitchenQuality"))
+                    .Append(context.Transforms.Categorical.OneHotEncoding("Foundation"))
+                    .Append(context.Transforms.Categorical.OneHotEncoding("Exterior1st"))
+                    .Append(context.Transforms.Categorical.OneHotEncoding("Exterior2nd"))
+                    //.Append(context.Transforms.Categorical.OneHotEncoding("YearSold"))
+                    .Append(context.Transforms.Categorical.OneHotEncoding("GarageFinish"))
+                    .Append(context.Transforms.Categorical.OneHotEncoding("GarageType"))
+                    //.Append(context.Transforms.Categorical.OneHotEncoding("FullBath"))
+                    .Append(context.Transforms.Concatenate(
+                        "Features",
+                        "LotAreaThousands",
+                        "OverallQuality",
+                        "OverallCondition",
+                        "YearBuilt",
+                        //"YearRemodAdd",
+                        ////"YearSold",
+                        //////"MSSubClass",
+                        "Neighbourhood",
+                        "TotalBasementSquareFeetThousands",
+                        "GroundLivingAreaThousands",
+                        "FullBath",
+                        //"TotalRoomsAboveGround",
+                        "BedroomsAboveGround",
+                        "GarageCars",
+                        "BuildingType",
+                        "HouseStyle",
+                        //"GarageType",
+                        //"KitchenQuality"
+                        //"GarageAreaThousands",
+                        //"GarageFinish",
+                        "Foundation"
+                        //"Exterior1st"
+                        //"Exterior2nd",
+                        //"LotFrontage"
+                        ))
+                    .AppendCacheCheckpoint(context)
+                    .Append(context.Regression.Trainers.FastTree());
+
+            Console.WriteLine("done!");
+
+            return pipeline;
+        }
+
+        private static ITransformer TrainModel(
+            MLContext context,
+            IDataView data,
+            IEstimator<ITransformer> pipeline)
+        {
+            Console.Write("Training the model...");
+            var partitions = context.Data.TrainTestSplit(data, testFraction: 0.2);
+            var model = pipeline.Fit(partitions.TrainSet);
+            var predictions = model.Transform(partitions.TestSet);
+
+            var metrics = context.Regression.Evaluate(predictions, "Label", "Score");
+
+            Console.WriteLine("done!");
+
+            Console.WriteLine();
+            Console.WriteLine($"Model metrics:");
+            Console.WriteLine($"  RMSE:{metrics.RootMeanSquaredError:#.##}");
+            Console.WriteLine($"  MSE: {metrics.MeanSquaredError:#.##}");
+            Console.WriteLine($"  MAE: {metrics.MeanAbsoluteError:#.##}");
+            Console.WriteLine();
+
+            return model;
+        }
+
+        private static void PredictPrices(
+            MLContext context,
+            ITransformer model)
+        {
+            var predictionFunction = 
+                context.Model.CreatePredictionEngine<HousePrice, HousePricePrediction>(model);
+
+
+            while (true)
+            {
+                Console.WriteLine("Iowa House Price Prediction");
+                var housePrice = new HousePrice();
+                Console.Write("Enter Lot Area: ");
+                housePrice.LotArea = Convert.ToSingle(Console.ReadLine());
+                Console.Write("Enter Overall Quality: ");
+                housePrice.OverallQuality = Convert.ToSingle(Console.ReadLine());
+                Console.Write("Enter Overall Condition: ");
+                housePrice.OverallCondition = Convert.ToSingle(Console.ReadLine());
+                Console.Write("Enter Year Built: ");
+                housePrice.YearBuilt = Convert.ToSingle(Console.ReadLine());
+                Console.Write("Enter Neighbourhood: ");
+                housePrice.Neighbourhood = Console.ReadLine();
+                Console.Write("Enter Basement Square Feet: ");
+                housePrice.TotalBasementSquareFeet = Convert.ToSingle(Console.ReadLine());
+                Console.Write("Enter Ground Living Area: ");
+                housePrice.GroundLivingArea = Convert.ToSingle(Console.ReadLine());
+                Console.Write("Enter Full Bathrooms: ");
+                housePrice.FullBath = Convert.ToSingle(Console.ReadLine());
+                Console.Write("Enter Bedrooms Above Ground: ");
+                housePrice.BedroomsAboveGround = Convert.ToSingle(Console.ReadLine());
+                Console.Write("Enter Garage Cars: ");
+                housePrice.GarageCars = Convert.ToSingle(Console.ReadLine());
+                Console.Write("Enter Building Type: ");
+                housePrice.BuildingType = Console.ReadLine();
+                Console.Write("Enter House Style: ");
+                housePrice.HouseStyle = Console.ReadLine();
+                Console.Write("Enter Foundation: ");
+                housePrice.Foundation = Console.ReadLine();
+
+                var prediction = predictionFunction.Predict(housePrice);
+
+                Console.WriteLine($"House price prediction: {prediction.HousePrice}");
+
+                Console.WriteLine("Predict again?");
+                if (Console.ReadLine() != "Y")
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
